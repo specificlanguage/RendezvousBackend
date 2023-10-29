2b99f47130ddcd834affc9a9a0d1b86fddb752aa
specificlanguage <specificlanguage@gmail.com>
2023-10-28 21:32:59 -0400

    feat(users): Get multiple users
    - Create POST endpoint on getUsers to get all users specified, useful for bulk trip info

--- src/user/get.ts
+++ src/user/get.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import { prisma } from "../db/prisma";
 
+interface GetUsersBody {
+    userIDs: string[];
+}
+
 export async function GetUser(req: Request, res: Response) {
     let userID: string = res.locals.userID;
     if (req.params.userID) {
@@ -19,5 +23,26 @@ export async function GetUser(req: Request, res: Response) {
         res.status(200).json(account);
     } else {
         res.status(500).json({ message: "Account not found" });
+    }
+}
+
+// NOTE: this uses body, so this must be post
+export async function GetUsers(req: Request, res: Response) {
+    const { userIDs } = req.body as GetUsersBody;
+
+    const accounts = await prisma.account.findMany({
+        where: {
+            id: {
+                in: userIDs,
+            },
+        },
+    });
+
+    if (accounts.length === 0) {
+        res.status(500).json({ message: "Accounts not found" });
+    } else if (accounts.length < userIDs.length) {
+        res.status(202).json(userIDs);
+    } else {
+        res.status(200).json(userIDs);
     }
 }
--- src/user/router.ts
+++ src/user/router.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { AuthMiddleware } from "./authorize";
 import { Signup } from "./signup";
-import { GetUser } from "./get";
+import { GetUser, GetUsers } from "./get";
 import { GetAllTrips } from "./getAllTrips";
 
 export const usersRouter = express.Router();
@@ -12,6 +12,7 @@ usersRouter.use(AuthMiddleware);
 usersRouter.get("/", GetUser);
 usersRouter.get("/get", GetUser);
 usersRouter.get("/get/:userID", GetUser);
+usersRouter.post("/getUsers", GetUsers);
 
 // Signups the user into the database (actual security does not happen here, it happens in firebase.)
 usersRouter.post("/signup", Signup);
