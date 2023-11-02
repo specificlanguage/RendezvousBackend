75794fbfadedf7f20835c98a9e83bb67a040950c
specificlanguage <specificlanguage@gmail.com>
2023-11-01 21:47:39 -0400

    fix: Don't show users if the user is not in trip

--- src/trip/getTrip.ts
+++ src/trip/getTrip.ts
@@ -1,18 +1,12 @@
 import { Request, Response } from "express";
-import { getAccount, isInTrip, prisma, tripExists } from "../db/prisma";
+import { isInTrip, prisma, tripExists } from "../db/prisma";
 
 export async function GetSingleTrip(req: Request, res: Response) {
     const userID = res.locals.userID;
-    const account = await getAccount(userID);
     const tripID = (req.query.tripID ?? "") as string;
 
     if (tripID === "" || !(await tripExists(tripID))) {
         res.status(500).json({ message: "Trip does not exist" });
-        return;
-    }
-
-    if (!(await isInTrip(account?.id ?? "", tripID ?? ""))) {
-        res.status(401).json({ message: "Unauthorized" });
         return;
     }
 
@@ -21,6 +15,11 @@ export async function GetSingleTrip(req: Request, res: Response) {
             id: tripID,
         },
     });
+
+    if (!trip) {
+        res.status(404).json({ message: "Trip not found" });
+        return;
+    }
 
     const locations = await prisma.location.findMany({
         where: {
@@ -44,9 +43,20 @@ export async function GetSingleTrip(req: Request, res: Response) {
         },
     });
 
-    res.status(200).json({
-        ...trip,
-        users,
-        locations: locations.map((loc) => loc.name),
-    });
+    const inTrip = await isInTrip(userID, tripID);
+
+    if (!inTrip) {
+        res.status(200).json({
+            ...trip,
+            users: users.filter((user) => trip?.adminID == user.id),
+            numUsers: users.length,
+            locations: locations.map((loc) => loc.name),
+        });
+    } else {
+        res.status(200).json({
+            ...trip,
+            users,
+            locations: locations.map((loc) => loc.name),
+        });
+    }
 }
